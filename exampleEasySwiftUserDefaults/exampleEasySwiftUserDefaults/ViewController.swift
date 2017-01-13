5724afab9047ac479fbe1305b0808a019b0d9a86
MaCMol90 <mauricio.correa@webprogramo.com>
2017-01-13 17:41:38 -0500

    cambios

---
+++ exampleEasySwiftUserDefaults/exampleEasySwiftUserDefaults/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  exampleEasySwiftUserDefaults
+//
+//  Created by Imac Webpro 2 on 13/01/17.
+//  Copyright © 2017 webprogramo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ exampleEasySwiftUserDefaults/exampleEasySwiftUserDefaults/easySwiftUserDefaults.swift
@@ -0,0 +1,13 @@
+//
+//  easySwiftUserDefaults.swift
+//  exampleEasySwiftUserDefaults
+//
+//  Created by Imac Webpro 2 on 13/01/17.
+//  Copyright © 2017 webprogramo. All rights reserved.
+//
+
+import Foundation
+
+class easySwiftUserDefaults {
+    
+}
