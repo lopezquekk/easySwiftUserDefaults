ee86fc5a68ea3c7c33467218fdb2cac7159a6308
Camilo López <lopezquekk@gmail.com>
2017-01-15 22:17:13 -0500

    First Commit
    Changes, Juan please complete what you want

--- exampleEasySwiftUserDefaults/exampleEasySwiftUserDefaults/easySwiftUserDefaults.swift
+++ exampleEasySwiftUserDefaults/exampleEasySwiftUserDefaults/easySwiftUserDefaults.swift
@@ -8,6 +8,68 @@
 
 import Foundation
 
-class easySwiftUserDefaults {
+open class easySwiftUserDefaults {
+    
+    
+    var defaults = UserDefaults.standard
+    //defaults.setObject("Coding Explorer", forKey: "userNameKey")
+    
+    init() {
+        
+    }
+    
+    func save(type:String,key:String, value:Any?)->Bool{
+        
+        switch type {
+            case "String":
+                defaults.set(value, forKey: key)
+            case "Bool":
+                defaults.set(value, forKey: key)
+            case "Integer":
+                defaults.set(value, forKey: key)
+            case "Double":
+                defaults.set(value, forKey: key)
+            case "Object":
+                defaults.set(value, forKey: key)
+            case "URL":
+                defaults.set(value, forKey: key)
+            default:
+                defaults.set(value, forKey: key)
+        }
+        
+       return true
+    }
+    
+    func change()->Bool{
+        
+        return true
+    }
+    
+    func getVal(type:String, key:String)->Any?{
+        
+        
+        let response:Any?
+        switch type {
+        case "String":
+            response  = defaults.string(forKey: key)
+        case "Bool":
+            response  = defaults.string(forKey: key)
+        case "Integer":
+            response  = defaults.string(forKey: key)
+        case "Double":
+            response  = defaults.string(forKey: key)
+        case "Object":
+            response  = defaults.string(forKey: key)
+        case "URL":
+            response  = defaults.string(forKey: key)
+        default:
+            response  = defaults.string(forKey: key)
+        }
+        
+        return response
+    }
+
+    
+    
     
 }
